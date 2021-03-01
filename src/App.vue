bc99c938b6c5cf784ba4879058f66ccf90850760
devcjlozano <carlosjlp@gmail.com>
2021-03-01 16:54:44 +0100

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <article :style="{ backgroundColor: color }">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <PersonalForm
+      :name="name"
+      :age="age"
+      :color="color"
+      @update:name="updateName"
+      @update:age="updateAge"
+      @update:color="updateColor"/> -->
+
+    <PersonalForm
+      :name="name"
+      :age="age"
+      :color="color"
+      v-model:name="name"
+      v-model:age="age"
+      v-model:color="color"/>
+   </article>
+</template>
+
+<script>
+import { ref } from 'vue'
+import PersonalForm from './components/PersonalForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PersonalForm
+  },
+  setup() {
+    const name = ref('John Doe')
+    const age = ref(33)
+    const color = ref('#FF0000')
+    
+    return { name, age, color}
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PersonalForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <article class="content">
+    <form class="form">
+      <div class="control">
+        <label for="name"> Nombre </label>
+        <input
+          :value="name"
+          @input="event => $emit('update:name', event.target.value)"
+          type="text"
+          id="name"
+          placeholder="Escribe tu nombre"/>
+      </div>
+
+      <div class="control">
+        <label for="name"> Edad </label>
+        <input
+          :value="age"
+          @input="event => $emit('update:age', event.target.value)"
+          type="number"
+          id="age"
+          placeholder="Escribe tu edad"/>
+      </div>
+
+      <div class="control">
+        <label for="colour"> Color favorito </label>
+        <input
+          :value="color"
+          @input="event => $emit('update:color', event.target.value)"
+          type="color"
+          id="color"
+          placeholder="Selecciona tu color favorito"/>
+      </div>
+    </form>
+  </article>
+
+</template>
+
+<script>
+export default {
+  name: 'PersonalForm',
+  props: ['name', 'color', 'age']
+}
+</script>
